524682181bd875763900588bf593aef8a8880797
Nguyễn Thái Son Ny <134358623+nguyenthaisonny@users.noreply.github.com>
2025-02-27 21:29:52 +1100

    add response messages module user

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -2,32 +2,38 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/
 import { UserService } from '@/module/user/user.service';
 import { CreateUserDto } from '@/module/user/dto/create-user.dto';
 import { UpdateUserDto } from '@/module/user/dto/update-user.dto';
+import { ResponseMessage } from '@/decorators/customs';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
   @Post()
+  @ResponseMessage("Create user.")
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
   @Get()
+  @ResponseMessage("Get all users.")
   findAll() {
     return this.userService.findAll();
   }
 
   @Get(':id')
+  @ResponseMessage("Get one user.")
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
   }
 
   @Put(':id')
+  @ResponseMessage("Update user.")
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ResponseMessage("Delete user.")
   remove(@Param('id') id: string) {
     return this.userService.remove(id);
   }
